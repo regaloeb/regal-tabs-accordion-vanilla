29cbf5ae4982c3c5971deeffa22545e907a96d10
regaloeb <loeb@regaloeb.com>
2020-02-17 18:38:58 +0100

    Maj diverses

--- index.php
+++ index.php
@@ -34,7 +34,8 @@
 				<p>
 					Si on veut des onglets en mode desktop et des accordéons en mode mobile, la solution consiste à charger les deux constructeurs, à appliquer une structure HTML adéquate et à déclarer les objets en fonction du contexte mobile/desktop.<br>
 					Certes, c'est un peu tordu mais cela fonctionne admirablement&nbsp;!<br><br>
-					<a href="https://github.com/regaloeb/regal-tabs-accordion-vanilla" target="_blank">Github repository</a>
+					<a href="https://github.com/regaloeb/regal-tabs-accordion-vanilla" target="_blank" class="picto-link"><span class="picto"> <svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>
+</span>Github repository</a>
 				</p>
 				<h2>HTML</h2>
 				<p>
@@ -93,10 +94,10 @@
 				<p>
 					Inclure les constructeurs en bas de page avant la fermeture du body&nbsp;:
 					<span class="code">
-&lt;script src="js/regal-tabs-vanilla.js">&lt;/script>
+&lt;script src="js/regal-tabs-vanilla.js">&lt;/script><br>
 &lt;script src="js/regal-accordion-vanilla.js">&lt;/script>
 					</span>
-					Déclarer les objets&nbsp;:
+					Puis déclarer les objets&nbsp;:
 					<span class="code">
 var tabsAccordion = document.querySelectorAll('.tabs-accordion');<br>
 if(tabsAccordion.length){<br>
